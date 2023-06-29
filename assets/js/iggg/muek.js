351a21f5f70d5ce252322db743a4cb0e58f95afd
Tim Domnick (2351453) <tim.domnick@student.kit.edu>
2023-06-29 20:02:03 +0200

    Create location format for MüK

---
+++ assets/js/iggg/muek.js
@@ -0,0 +1,7 @@
+export const name = 'Mensa über [kœri]werk';
+export const key = 'muek';
+export const regex = /^M(ü|ue)K(1(,2)?|2(,3)?|3(,4)?|4(,5)?|5(,6)?|6(,7)?|7(,8)?|8(,9)?|9(,10)?|10(,11)?|11(,12)?|12)([ai]?)$/i;
+export const groups = {
+  angle: 2,
+  ring: 14,
+};
