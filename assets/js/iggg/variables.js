5095b262a34679f8138afce5266887433a3b7c74
Tim Domnick (2351453) <tim.domnick@student.kit.edu>
2023-07-16 22:19:26 +0200

    Add styling variables for iggg maps

---
+++ assets/js/iggg/variables.js
@@ -0,0 +1,7 @@
+export const locationFill = '#689F38';
+export const locationStroke = '#1B5E20';
+export const highlightFill = '#FFA000';
+export const highlightStroke = '#E65100';
+export const outlineStroke = '#004D40';
+export const markersFill = '#00695C';
+export const font = 'bold 0.8em "Skranji", sans-serif';
